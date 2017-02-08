2a3639526f93cd03ba46a0b0702644827d388d9d
Anthony Hom <anthonyhom2@hotmail.com>
2017-02-08 00:07:03 -0500

    Done organizing the other sqrt fxn but not sure whether isCloseEnough from anotherSqrt should contain more content

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,29 @@
+public class Recursion{
+	public static String name(){
+		return "Hom,Anthony";
+	}
+	public static double sqrt(double n){
+		if (n < 0){
+			throw new IllegalArgumentException("Sorry only values greater than 0");
+		}
+		else {
+			return anotherSqrt(n, 1.0);
+		}
+	}
+
+	public static double anotherSqrt(double n, double guess){
+		double betterGuess = (n / guess + guess) / 2;
+		//Checks for the percent difference between the two values given;
+		if(Math.abs(n - betterGuess * betterGuess) / n <= 0.00000000001){
+			return betterGuess;
+		}
+		else{
+			return anotherSqrt(n, betterGuess);
+		}
+	}
+
+	public static void main(String[]args){
+		System.out.println(anotherSqrt(100, 1));
+		System.out.println(sqrt(9));
+	}
+}
