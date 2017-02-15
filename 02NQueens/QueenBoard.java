3d6e6245a0a1798482813907ef1443c616914376
Anthony Hom <anthonyhom2@hotmail.com>
2017-02-14 23:30:59 -0500

    I simplified the direction in how we add/remove queen which caused problems initially for count and such but it works now

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,13 +1,13 @@
 public class QueenBoard{
 	private int[][] board;
-	private int solutionCount;
+	private int solutionCount = -1;
 
 	public QueenBoard(int size){
 		board = new int[size][size];
 	}
 
 	public boolean solve(){
-		solveH(0);
+		return solveH(0);
 	}
 
 	private boolean solveH(int col){
@@ -15,7 +15,7 @@ public class QueenBoard{
 			return true;
 		}
 		for(int row = 0; row < board.length; row++){
-			if(board[row][col] == 0;){
+			if(board[row][col] == 0){
 				addQueen(row, col);
 				if (solveH(col + 1)){
 					return true;
@@ -29,16 +29,104 @@ public class QueenBoard{
 	}
 
 	private boolean addQueen(int row, int col){
-		for(int index = 0; index < board.length; index++){
-			board[row][index] = 1;
-			board[index][col] = 1;
-		for(int r = row; r < board.length; r++){
-			if(board[r][col] = 0){
-				board[r][col] = "Q";
-				board[r++][col] += 1;
-		for 
-;}
-	private boolean removeQueen(int row, int col){
+		if (board[row][col] != 0){
+	    return false;
+		}
+		board[row][col] = 1;
+		int index = 1;
+		while (col + index < board[row].length){
+	    	board[row][col + index]--;
+	    	if (row - index >= 0){
+				board[row - index][col + index]--;
+	    	}
+	    	if (row + index < board.length){
+				board[row+index][col+index]--;
+	    	}
+	    index++;
+	}
+		return true;
+    }
 
-;}
+	private boolean removeQueen(int row, int col){
+		if (board[row][col] != 1){
+	    return false;
+	}
+	board[row][col] = 0;
+	int index = 1;
+	while (col + index < board[row].length){
+	    board[row][col + index]++;
+	    if(row - index >= 0){
+		board[row - index][col + index]++;
+	    }
+	    if(row + index < board.length){
+		board[row + index][col + index]++;
+	    }
+	    index++;
+	}
+	return true;
+    }
+
+    //Help f/ Mr. K + Alex S. to keep track of solutionCount as the functions recurse
+    public void countSolutions(){
+	for(int row = 0; row < board.length; row++){
+	    for(int col = 0; col < board.length; col++){
+		board[row][col] = 0;
+	    }
+	}
+	solutionCount = 0;
+	countH(0);
+	    }
+    
+    private boolean countH(int col){
+	if(board.length == 2 || board.length == 3){
+	    return false;
+	}
+	for(int row = 0; row < board.length; row++){
+	    if(col >= board.length){
+		return true;
+	    }
+	    if(addQueen(row, col)){
+		addQueen(row, col);
+		if(countH(col + 1)){
+		    solutionCount++;
+		    removeQueen(row, col);
+		}
+		else{
+		    removeQueen(row, col);
+		}
+	    }
+	}
+	return false;
+    }
+    
+    public int getSolutionCount(){
+	return solutionCount;
+    }
+
+
+    public String toString(){
+    	String result = "";
+	for(int row = 0; row < board.length; row++){
+	    for(int col = 0; col < board.length; col++){
+		if (board[row][col] == -1){
+		    result += "Q ";
+		}
+		else{
+		    result += "_ ";
+		}
+		if(col == board.length - 1){
+		    result += "\n";
+		}
+	    }
+	}
+	return result;	    
+    }
+
+    public static void main(String[] args){
+	QueenBoard board = new QueenBoard(10);
+	board.solve();
+	System.out.println(board);
+	//board.countSolutions();
+	System.out.println(board.getSolutionCount());
+    }
 }
