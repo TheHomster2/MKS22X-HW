00ebed7e3b5dbdb3acb43c8d7440bfb9618157e6
Anthony Hom <anthonyhom2@hotmail.com>
2017-02-16 14:50:42 -0500

    Update

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -3,15 +3,30 @@ private int[][] board;
 private String string="";
 
 	public KnightBoard(int startingRows, int startingCols)
+
 	public String toString(){
-	if(
+		String result = "";
+	for(int row = 0; row < board.length; row++){
+		for(int col = 0; col < board.length; col++){
+			if(board[row][col] < 10){
+				result+= board[row][col];
+			}
+			else {
+				result += board[row][col] + " ";
+				}
 	return string;
 	}
+
 	public void solve(){
 	return solveH(0,0,0);
+	}
+
 	private boolean solveH(int row, int col, int level){
 		if (level > board.length * board[row]){
 			return true;
 		}
-		if 
+		if (board[row][col] != 0){
+			return false;
+		}
+		if (board[row + 1][col] 
 	}
