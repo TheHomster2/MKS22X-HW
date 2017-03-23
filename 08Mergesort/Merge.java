6a8dcaccee91b2e414a5e2a607edba02dbd84561
Anthony Hom <anthonyhom2@hotmail.com>
2017-03-23 14:48:53 -0400

    Started

---
+++ 08Mergesort/Merge.java
@@ -0,0 +1,18 @@
+public class Merge{
+
+	public static void mergesort(int[] ary){
+		if(ary.length != 1){
+		int temp = new int [ary.length / 2];
+	}
+
+	public void merge(int[] a, int[] b,int[] destination){
+		int lo = 0;
+		int hi = 0;
+		for (int index = 0; index < destination.length; index++){
+			if(a[index] < b[index]){
+				destination[index] = a[index];
+			}
+			for (int middle = index + 1; middle < destination.length; middle++){
+				
+		}	
+	}
