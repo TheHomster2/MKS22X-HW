7795f9a62b9524e5675543f305c1bd4d4ea5ce3b
Anthony Hom <anthonyhom2@hotmail.com>
2017-03-27 23:05:38 -0400

    Did merge sort with helper fxn

--- 08Mergesort/Merge.java
+++ 08Mergesort/Merge.java
@@ -1,9 +1,30 @@
 public class Merge{
 
 	public static void mergesort(int[] ary){
-		if(ary.length != 1){
-		int temp = new int [ary.length / 2];
+		ary = mergesortH(ary);
 	}
+
+	public static void mergesortH(int[] ary){
+		if(ary.length <= 1){
+			return ary;
+		}
+
+		int [] left = new int [ary.length / 2];
+		int [] right = new int[ary.length - left.length];
+		for(int index = 0; index < ary.length; index++){
+			if(index < left.length){
+				left[index] = ary[index];
+			}
+			else{
+				right[index - left.length] = data[index];
+			}
+		}
+
+		mergesortH(left);
+		mergesortH(right);
+		merge(left, right, ary);
+	}
+
 
 	public void merge(int[] a, int[] b,int[] destination){
 		int lo = 0;
@@ -29,7 +50,7 @@ public class Merge{
 			lo++;
 			}
 		}	
-		
+
 		while(hi < b.length){
 			for(int index = lo + hi; index < destination.length; index++){
 			destination[index2] = b[hi];
