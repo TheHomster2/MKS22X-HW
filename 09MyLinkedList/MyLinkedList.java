6ec1dbba540a765a4bccdabdd6c1181694976ae4
Anthony Hom <anthonyhom2@hotmail.com>
2017-03-30 10:53:29 -0400

    Built a sort of add pseudo code for linked lists but just need to make some corrections

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -9,15 +9,36 @@ public class MyLinkedList{
 	LNode index = new LNode(value);
 	if (start == null){
 		start = index;
-}
+	}
 	LNode current = start;
 		while (current.hasNext() != null){
 			current = current.next();
 			size++
 		}
-
+	return false;
 	}
-}	
+	
+
+	public void add(int index, int value){
+	if(index < 0 || index > size()){
+		throw new IndexOutOfBoundsException();
+	}
+	if(index == size()){
+	add(value);
+	return;
+}		
+	if(index == 0){
+	index == new LNode(value);
+	}
+	else{
+		LNode current = index;
+		for(int i = 0; i < index; i++){
+			current = current.Next();
+		}
+	current.setNext(current.Next());
+	}	
+	size++;
+}
 	public int size(){
 	return size;
 }
