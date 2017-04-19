28c91c6b5c7010dc7f4e4ca2c1e8d2e770fc3c80
Anthony Hom <anthonyhom2@hotmail.com>
2017-04-18 20:44:26 -0400

    Worked on add, remove, and get methods. Still have a few methods to write up

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,9 +1,128 @@
 import java.util.*;
 import java.io.*;
 
-public class MyLinkedList{
+public class MyLinkedList implements Iterator<Integer>{
 
-	public LinkedList(){
+	private class LNode{
+		Lnode next, prev;
+		int value;
+
+		public LNode(int value){
+			next = null;
+			prev = null;
+			this.value = value;
+		}
+
+		public String toString(){
+			return value + "";
+		}
+	}
+
+	LNode start, tail;
+	int size;
+
+	public MyLinkedList(){
+		start = null;
+		tail = null;
+		size = 0;
+	}
+
+	public int size(){
+		return size;
+	}
+
+	public boolean add(int value){
+		LNode index = new LNode(value);
+		if (size < 1){
+		start = index;
+		tail = index;
+		}
+
+		else if (size == 1) {
+	    start.next = n;
+	    index.prev = start;
+	    tail = index;
+	}
+	else {
+	    tail.next = index;
+	    index.prev = tail;
+	    tail = index;
+	}
+	size++;
+	return true;
+	}
+
+	public void add(int index, int value){
+		if (index < 0 || index > size()){
+		throw new IndexOutOfBoundsException();
+		}
+
+		LNode input = new LNode(value);
+		if (index == size()){
+		add(value);
+		}	
+		if (index == 0){
+			input.setNext(start);
+			start = input;
+			size++;
+		}
+		LNode current = start;
+		for (int track = 0; track < index - 1; track++){
+			current = current.getNext();
+			}
+		input.setNext(current.getNext());
+		current.setNext(input);
+		size++;
+	}
+
+	private LNode getNthNode(int n){
+		LNode current = start;
+		for(int index = 0; index < n; index++){
+			current = current.getNext();
+		}
+		return current;
+	}
+
+	public int remove(int index){
+		LNode target = getNthNode(index);
+		remove(target);
+		size--;
+		return target.value;
+	}
+
+	private void remove(LNode target){
+		if (target == start) {
+	    start = target.next;
+	}
+	else if (target == tail) {
+	    tail = target.prev;
+	}
+	else {
+	    target.prev.next = target.next;
+	    target.next.prev = target.prev;
+		}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 	private LNode start;
 	private int size;
@@ -95,3 +214,4 @@ public class MyLinkedList{
 			}	
 }
 }
+*/
