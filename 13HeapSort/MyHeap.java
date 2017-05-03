7cf3fa471ad803d22e175637fdca8faf7e009da1
Anthony Hom <anthonyhom2@hotmail.com>
2017-05-03 14:49:25 -0400

    Start on heap sort

---
+++ 13HeapSort/MyHeap.java
@@ -0,0 +1,18 @@
+public class MyHeap{
+
+public MyHeap(){}
+
+public MyHeap(boolean result){
+	if (result = true){
+	r	
+		}
+
+public void add(String s){}
+
+public String remove(){}
+
+public String peek(){
+
+private pushUp(){}
+
+private pushDown(){}
