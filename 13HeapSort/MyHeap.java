09185855affa12d8909e61deb92d42b0ab537fb1
Anthony Hom <anthonyhom2@hotmail.com>
2017-05-05 00:21:44 -0400

    Completed required methods

--- 13HeapSort/MyHeap.java
+++ 13HeapSort/MyHeap.java
@@ -1,44 +1,88 @@
-import.java.util.*;
+import java.util.*;
 
 public class MyHeap{	
 
 private ArrayList<String> heap;
+private int size;
+private int constant;
 
 public MyHeap(){
-heap = new ArrayList<String>();
+	heap = new ArrayList<String>();
+	size = 0;
+	heap.add("");
 }
 
 public MyHeap(boolean result){
-if (result){
-	MyHeap();
+	if (result){
+		constant = 1;
 	}
-else{
+	else{
+		constant = -1;
+	}
 	heap = new ArrayList<String>();
+	heap.add("");
+}
 
 public void add(String s){
-pushUp()
+	size+=1;
+	heap.add(s);
+	pushUp(size);
 }
 
-public String remove(){}
+public String remove(){
+	String temp = heap.get(size);
+	heap.set(1, heap.remove(size));
+	size -= 1;
+	pushDown(1);
+	return temp;
+}
 
 public String peek(){
-	heap.get(1);
+	return heap.get(1);
 }
 
-private void pushUp(){
-for (int index = 1; index < heap.length; index++){
-	if ((heap.get(index)) > heap.get(2 * index)){
-
-}
-}
-
-private void pushDown(){
-for (int index = 1; index < heap.length; index++){
-	if ((heap.get(index)) < heap.get(2 * index)){
-	temp = heap.get(index);
-	heap.get(i) = heap.get(2 * index);
-	heap.get(2 * index) = temp;
+private void pushUp(int index){
+	if (index == 1){
+		return;
 	}
 
+	if (size >= 1 && index > 1 && heap.get(index).compareTo(heap.get(index / 2)) < 0){
+		return;
+	}
+
+	else{
+		swap(index, index / 2);
+		pushUp(index / 2);
+	}
 }
+
+private void pushDown(int index){
+	if (constant == 1){
+		if (index < size && heap.get(index).compareTo(heap.get(2 * index)) < 0){
+			swap(index, 2 * index);
+			pushDown(2 * index);
+		}
+	}
+	else{
+		if (index > 1 && heap.get(index).compareTo(heap.get(2 * index)) > 0){
+			swap(index, 2 * index);
+			pushDown(2 * index);
+		}
+	}
+}
+
+public void swap(int index, int index2){
+	String temp = heap.get(index);
+	heap.set(index, heap.get(index2));
+	heap.set(index2, temp);
+}
+
+ public String toString(){
+	String result = "";
+	for(int index = 1; index < heap.size(); index++){
+	    result += heap.get(index) + ", ";
+	}
+	return result;
+    }
+
 }
